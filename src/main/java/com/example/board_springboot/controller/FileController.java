b87a471691c89478e5dc21edeb38d2bf0f561d51
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-10 03:26:21 +0900

    파일 다운로드 예시 추가

--- src/main/java/com/example/board_springboot/controller/FileController.java
+++ src/main/java/com/example/board_springboot/controller/FileController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.*;
 
 @Slf4j
